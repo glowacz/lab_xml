99ef82e758d8011bc56c942511ab6e2e8f7554ce
glowacz <glowacki.pj@gmail.com>
2023-03-10 01:35:43 +0100

    Add project files.

---
+++ app/Program.cs
@@ -0,0 +1,31 @@
+﻿using Library;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\glowa\\source\\repos\\ProjOb\\lab2_cs\\lab2_cs\\library.xml";
+            typeLib lib = LibraryReader.ReadLibrary(path);
+            foreach (var item in lib.book)
+            {
+                Console.WriteLine(item.title);
+
+                foreach (var aut in item.authorr)
+                {
+                    foreach (var aut_main in lib.author)
+                    {
+                        //Console.WriteLine(aut_main.surname + aut_main.surname.Length);
+                        if (aut_main.id == aut.id)
+                        {
+                            Console.WriteLine(string.Join(", ", aut_main.name) + " " + aut_main.surname);
+                            //Console.WriteLine(aut_main.surname);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ lab2_cs/Class1.cs
@@ -0,0 +1,69 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace ClassLibrary1
+{
+    public static class LibraryReader
+    {
+        public static typeLib ReadLibrary(string path)
+        {
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            // Ustawienia walidacji
+            settings.ValidationType = ValidationType.Schema;
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessInlineSchema;
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+
+            // Dodanie pliku xsd dla danej przestrzeni nazw
+            // Chcemy dodać zdefiniowaną w zadaniu przestrzeń. 
+            // (Tak jak w ustawieniach XML -> Schemas w VS
+            settings.Schemas.Add("http://example.org/jk/library", "C:\\Users\\glowa\\source\\repos\\ProjOb\\lab2_cs\\lab2_cs\\LibrarySchema.xsd");
+
+            // Przetwarzanie schemaLocation z XSI
+            // Domyślnie wyłączone ze względów bezpieczeństwa
+            // (można zmusić program do przeczytania dowolnego pliku). 
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessSchemaLocation;
+
+            // Funkcja, która będzie obsługiwać pojawiające się błędy walidacji
+            // Może zostać wywołana wielokrotnie dla jednego dokumentu
+            settings.ValidationEventHandler += ValidationHandler;
+
+            //XmlReader reader = XmlReader.Create(path, settings);
+
+            // Read odczytuje kolejny element/atrybut z dokumentu. 
+            // Moze spowodować wywołanie delegacji przypiętej do ValidationEventHandler. 
+            //while (reader.Read())
+            //{
+            //}
+            XmlReader reader = XmlReader.Create(path, settings);
+            while (reader.Read()) { }
+
+            XmlSerializer serializer = new XmlSerializer(typeof(typeLib));
+             typeLib lib;
+            using (reader = XmlReader.Create(path))
+            {
+                lib = (typeLib)serializer.Deserialize(reader);
+            }
+            return lib;
+        }
+
+        private static void ValidationHandler(Object sender, ValidationEventArgs args)
+        {
+            if (args.Severity == XmlSeverityType.Warning)
+                Console.WriteLine("Warning: {0}", args.Message);
+            else
+                Console.WriteLine("Error: {0}", args.Message);
+        }
+    }
+    public class Class1
+    {
+
+    }
+}
